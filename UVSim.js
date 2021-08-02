a44708b290976a6a5af8ab1e5b3918967b8a64d0
Brandon Horlacher <bhorlacher@tangynt.com>
2021-08-02 15:09:44 -0600

    finishing program and adding tests.

--- UVSim.js
+++ UVSim.js
@@ -1,221 +1,224 @@
 // setup to turn JS into a console program
 class UVSim {
-	constructor() {
-		this.rl = require("readline");
-		this.prompts = this.rl.createInterface(process.stdin, process.stdout);
-		this.readlineSync = require('readline-sync');
+    constructor() {
+        this.rl = require("readline");
+        this.prompts = this.rl.createInterface(process.stdin, process.stdout);
+        this.readlineSync = require('readline-sync');
 
-		// where users input instructions are stored
-		this.memorySize = 100;
-		this.memory = [];
-		for (let i = 0; i < this.memorySize; i++) {
-				this.memory.push(0);
-		}
-		this.instructionCounter = 0;
-		this.accumulator = 0;
-		this.instructionRegister = 0;
-		this.opCode = 0;
-		this.operand = 0;
-		this.validOpCodes = [10,11,20,21,30,31,32,33,40,41,42,43];
-	}
+        // where users input instructions are stored
+        this.memorySize = 100;
+        this.memory = [];
+        for (let i = 0; i < this.memorySize; i++) {
+            this.memory.push(0);
+        }
+        this.instructionCounter = 0;
+        this.accumulator = 0;
+        this.instructionRegister = 0;
+        this.opCode = 0;
+        this.operand = 0;
+        this.validOpCodes = [10, 11, 20, 21, 30, 31, 32, 33, 40, 41, 42, 43];
+    }
 
-	initializeProgram() {
-			this.displayUserInstructions();
-			this.getUserInstructions();
-	}
+    initializeProgram() {
+        this.displayUserInstructions();
+        this.getUserInstructions();
+    }
 
-	displayUserInstructions() {
-			const userInstructions = 'Please enter your program one instruction at a time into the input. Enter -99999 to stop entering your program.';
-			console.log(userInstructions);
-	}
+    displayUserInstructions() {
+        const userInstructions = 'Please enter your program one instruction at a time into the input. Enter -99999 to stop entering your program.';
+        console.log(userInstructions);
+    }
 
-	// recursively loads user instruction into memory
-	// written by Brandon Horlacher
-	getUserInstructions() {
-			this.prompts.question(this.formatNumber(2, this.instructionCounter) + ' ? ', (response) => {
-					// recursively call to get another instruction until the user inputs the instruction indicating they are done
-					if (response != '-99999' && this.instructionCounter < this.memorySize) {
-							if (this.validateInstruction(response)) {
-									this.memory[this.instructionCounter] = Number.parseInt(response);
-									this.instructionCounter++;
-							} else {
-									console.log('That was not a valid instruction.');
-							}
+    // recursively loads user instruction into memory
+    // written by Brandon Horlacher
+    getUserInstructions() {
+        this.prompts.question(this.formatNumber(2, this.instructionCounter) + ' ? ', (response) => {
+            // recursively call to get another instruction until the user inputs the instruction indicating they are done
+            if (response != '-99999' && this.instructionCounter < this.memorySize) {
+                if (this.validateInstruction(response)) {
+                    this.memory[this.instructionCounter] = Number.parseInt(response);
+                    this.instructionCounter++;
+                } else {
+                    console.log('That was not a valid instruction.');
+                }
 
-							this.getUserInstructions();
-					} else {
-							this.prompts.close();
-							this.executeProgram();
-					}
-			});
-	}
+                this.getUserInstructions();
+            } else {
+                this.prompts.close();
+                this.executeProgram();
+            }
+        });
+    }
 
-	// validate the input instruction was valid
-	// written by Benjamin Larsen
-	validateInstruction(instruction) {
-		if (!instruction.match(/^\+\d{4}$/)) return false
-		const checkOpCode = instruction.substring(1,3)
-		if (!this.validOpCodes.includes(parseInt(checkOpCode))) return false
-		return true;
-	}
+    // validate the input instruction was valid
+    // written by Benjamin Larsen
+    validateInstruction(instruction) {
+        if (!instruction.match(/^\+\d{4}$/)) return false
+        const checkOpCode = instruction.substring(1, 3)
+        if (!this.validOpCodes.includes(parseInt(checkOpCode))) return false
+        return true;
+    }
 
-	// execute the program input by the user and print the state of registers and memory after
-	executeProgram() {
-			this.performInstructions();
-			this.printRegisters();
-			this.printMemory();
+    // execute the program input by the user and print the state of registers and memory after
+    executeProgram() {
+        this.performInstructions();
+        this.printRegisters();
+        this.printMemory();
 
-			process.exit();
-	}
+        process.exit();
+    }
 
-	// perform the operations that were input by the user
-	// written by Brandon Horlacher
-	performInstructions() {
-			while (this.opCode !== 43) {
-					this.loadNextInstruction();
+    // perform the operations that were input by the user
+    // written by Brandon Horlacher
+    performInstructions() {
+        while (this.opCode !== 43) {
+            this.loadNextInstruction();
 
-					switch (this.opCode) {
-							case 10: // read
-									this.read(this.operand);
-									break;
-							case 11: // write
-									this.write(this.operand);
-									break;
-							case 20: // load
-									this.load(this.operand);
-									break;
-							case 21: // store
-									this.store(this.operand);
-									break;
-							case 30: // add
-									this.add(this.operand);
-									break;
-							case 31: // subtract
-									this.subtract(this.operand);
-									break;
-							case 32: // divide
-									this.divide(this.operand);
-									break;
-							case 33: // multiply
-									this.multiply(this.operand);
-									break;
-							default:
-									break;
-					}
-			}
-	}
+            switch (this.opCode) {
+                case 10: // read
+                    this.read(this.operand);
+                    break;
+                case 11: // write
+                    this.write(this.operand);
+                    break;
+                case 20: // load
+                    this.load(this.operand);
+                    break;
+                case 21: // store
+                    this.store(this.operand);
+                    break;
+                case 30: // add
+                    this.add(this.operand);
+                    break;
+                case 31: // subtract
+                    this.subtract(this.operand);
+                    break;
+                case 32: // divide
+                    this.divide(this.operand);
+                    break;
+                case 33: // multiply
+                    this.multiply(this.operand);
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
 
-	// load the next instruction, opcode, and operand into the registers
-	// written by Brandon Horlacher
-	loadNextInstruction() {
-			switch (this.opCode) {
-					case 0:
-							this.instructionCounter = 0;
-							break;
-					case 40:
-							this.instructionCounter = this.operand;
-							break;
-					case 41:
-							if (this.accumulator < 0) {
-									this.instructionCounter = this.operand;
-							} else {
-									this.instructionCounter++;
-							}
-							break;
-					case 42:
-							if (this.accumulator === 0) {
-									this.instructionCounter = this.operand;
-							} else {
-									this.instructionCounter++;
-							}
-							break;
-					default:
-							this.instructionCounter++;
-							break;
-			}
+    // load the next instruction, opcode, and operand into the registers
+    // written by Brandon Horlacher
+    loadNextInstruction() {
+        switch (this.opCode) {
+            case 0:
+                this.instructionCounter = 0;
+                break;
+            case 40:
+                this.instructionCounter = this.operand;
+                break;
+            case 41:
+                if (this.accumulator < 0) {
+                    this.instructionCounter = this.operand;
+                } else {
+                    this.instructionCounter++;
+                }
+                break;
+            case 42:
+                if (this.accumulator === 0) {
+                    this.instructionCounter = this.operand;
+                } else {
+                    this.instructionCounter++;
+                }
+                break;
+            default:
+                this.instructionCounter++;
+                break;
+        }
 
-			this.instructionRegister = this.memory[this.instructionCounter];
-			this.opCode = Number.parseInt(('' + this.instructionRegister).substring(0, 2));
-			this.operand = Number.parseInt(('' + this.instructionRegister).substring(2, 4));
-	}
+        this.instructionRegister = this.memory[this.instructionCounter];
+        this.opCode = Number.parseInt(('' + this.instructionRegister).substring(0, 2));
+        this.operand = Number.parseInt(('' + this.instructionRegister).substring(2, 4));
+    }
 
 
-	// written by Benjamin Larsen
-	// input allows override of reading from console for testing
-	read(operand, input = null) {
-		const response = input || this.readlineSync.question("Enter an integer: ")
-		this.memory[operand] = response
-	}
+    // written by Benjamin Larsen
+    // input allows override of reading from console for testing
+    read(operand, input = null) {
+        const response = input || this.readlineSync.question("Enter an integer: ")
+        this.memory[operand] = response
+    }
 
-	// written by Benjamin Larsen
-	write(operand) {
-		console.log(this.formatNumber(5,parseInt(this.memory[operand])))
-	}
+    // written by Benjamin Larsen
+    write(operand) {
+        console.log(this.formatNumber(5, parseInt(this.memory[operand])))
+    }
 
-	// written by Benjamin Larsen
-	load(operand) {
-		this.accumulator = this.memory[operand]
-	}
+    // written by Benjamin Larsen
+    load(operand) {
+        this.accumulator = this.memory[operand]
+    }
 
-	// written by Benjamin Larsen
-	store(operand) {
-		this.memory[operand] = this.accumulator
-	}
+    // written by Benjamin Larsen
+    store(operand) {
+        this.memory[operand] = this.accumulator
+    }
 
-	add(operand) {
-			// todo
-	}
+    // written by Brandon Horlacher
+    add(operand) {
+        this.accumulator += this.memory[operand]
+    }
 
-	subtract(operand) {
-			// todo
-	}
+    // written by Brandon Horlacher
+    subtract(operand) {
+        this.accumulator -= this.memory[operand]
+    }
 
-	divide(operand) {
-			// todo
-	}
+    // written by Brandon Horlacher
+    divide(operand) {
+        this.accumulator /= this.memory[operand]
+    }
 
-	multiply(operand) {
-			// todo
-	}
+    // written by Brandon Horlacher
+    multiply(operand) {
+        this.accumulator *= this.memory[operand]
+    }
 
-	// print out the current state of the registers
-	// written by Brandon Horlacher
-	printRegisters() {
-			console.log();
-			console.log('REGISTERS:');
-			console.log('Accumulator:           ' + this.formatNumber(5, this.accumulator));
-			console.log('InstructionCounter:       ' + this.formatNumber(2, this.instructionCounter));
-			console.log('InstructionRegister:   ' + this.formatNumber(5, this.instructionRegister));
-			console.log('OperationCode:            ' + this.formatNumber(2, this.opCode));
-			console.log('Operand:                  ' + this.formatNumber(2, this.operand));
-			console.log();
-	}
+    // print out the current state of the registers
+    // written by Brandon Horlacher
+    printRegisters() {
+        console.log();
+        console.log('REGISTERS:');
+        console.log('Accumulator:           ' + this.formatNumber(5, this.accumulator));
+        console.log('InstructionCounter:       ' + this.formatNumber(2, this.instructionCounter));
+        console.log('InstructionRegister:   ' + this.formatNumber(5, this.instructionRegister));
+        console.log('OperationCode:            ' + this.formatNumber(2, this.opCode));
+        console.log('Operand:                  ' + this.formatNumber(2, this.operand));
+        console.log();
+    }
 
-	// print out the current contents of the memory
-	// written by Brandon Horlacher
-	printMemory() {
-			console.log('MEMORY:')
-			console.log('       00     01     02     03     04     05     06     07     08     09');
-			for (let i = 0; i < 10; i++) {
-					let memoryString = '';
-					memoryString += this.formatNumber(2, (i * 10));
+    // print out the current contents of the memory
+    // written by Brandon Horlacher
+    printMemory() {
+        console.log('MEMORY:')
+        console.log('       00     01     02     03     04     05     06     07     08     09');
+        for (let i = 0; i < 10; i++) {
+            let memoryString = '';
+            memoryString += this.formatNumber(2, (i * 10));
 
-					for (let j = 0; j < 10; j++) {
-							memoryString += '  ' + this.formatNumber(5, this.memory[(i * 10) + j]);
-					}
+            for (let j = 0; j < 10; j++) {
+                memoryString += '  ' + this.formatNumber(5, this.memory[(i * 10) + j]);
+            }
 
-					console.log(memoryString);
-			}
-	}
+            console.log(memoryString);
+        }
+    }
 
-	// format a number value with the given number of minimum digits
-	formatNumber(digits, value) {
-			return parseInt(value).toLocaleString('en-US', {
-					minimumIntegerDigits: digits,
-					useGrouping: false
-			});
-	}
+    // format a number value with the given number of minimum digits
+    formatNumber(digits, value) {
+        return parseInt(value).toLocaleString('en-US', {
+            minimumIntegerDigits: digits,
+            useGrouping: false
+        });
+    }
 }
 
-// exports.initializeProgram = initializeProgram;
 module.exports = UVSim;
--- UVSim.test.js
+++ UVSim.test.js
@@ -1,37 +1,183 @@
 const UVSim = require('./UVSim.js')
-const uvsim = new UVSim()
+const uvSim = new UVSim()
 
-test('read exported successfully', () => {
-	expect(typeof uvsim.read).toBe('function')
+test('test display user instructions', () => {
+	const consoleSpy = jest.spyOn(console, 'log')
+	uvSim.displayUserInstructions()
+	expect(consoleSpy).toHaveBeenCalledWith('Please enter your program one instruction at a time into the input. Enter -99999 to stop entering your program.')
+})
+
+test('+1010 is valid instruction', () => {
+	expect(uvSim.validateInstruction('+1010')).toBe(true)
+})
+
+test('+10101 is invalid instruction', () => {
+	expect(uvSim.validateInstruction('+10101')).toBe(false)
+})
+
+test('test 43 quits program', () => {
+	uvSim.opCode = 43;
+
+	const spy = jest.spyOn(uvSim, 'loadNextInstruction')
+	uvSim.performInstructions()
+	expect(spy).not.toBeCalled()
+})
+
+test('load first instruction', () => {
+	uvSim.instructionCounter = 0
+	uvSim.opCode = 0
+	uvSim.memory[0] = +1020
+
+	uvSim.loadNextInstruction()
+
+	expect(uvSim.instructionCounter).toBe(0)
+	expect(uvSim.instructionRegister).toBe(1020)
+	expect(uvSim.opCode).toBe(10)
+	expect(uvSim.operand).toBe(20)
+})
+
+test('load next normal instruction', () => {
+	uvSim.instructionCounter = 0
+	uvSim.opCode = 10
+	uvSim.memory[1] = +2030
+
+	uvSim.loadNextInstruction()
+
+	expect(uvSim.instructionCounter).toBe(1)
+	expect(uvSim.instructionRegister).toBe(2030)
+	expect(uvSim.opCode).toBe(20)
+	expect(uvSim.operand).toBe(30)
+})
+
+test('load next branch instruction', () => {
+	uvSim.instructionCounter = 0
+	uvSim.opCode = 40
+	uvSim.operand = 30
+	uvSim.memory[30] = +3020
+
+	uvSim.loadNextInstruction()
+
+	expect(uvSim.instructionCounter).toBe(30)
+	expect(uvSim.instructionRegister).toBe(3020)
+	expect(uvSim.opCode).toBe(30)
+	expect(uvSim.operand).toBe(20)
+})
+
+test('load next branchNeg branch instruction', () => {
+	uvSim.instructionCounter = 0
+	uvSim.opCode = 41
+	uvSim.operand = 25
+	uvSim.accumulator = -1234
+	uvSim.memory[25] = +2320
+
+	uvSim.loadNextInstruction()
+
+	expect(uvSim.instructionCounter).toBe(25)
+	expect(uvSim.instructionRegister).toBe(2320)
+	expect(uvSim.opCode).toBe(23)
+	expect(uvSim.operand).toBe(20)
+})
+
+test('load next branchNeg no branch instruction', () => {
+	uvSim.instructionCounter = 0
+	uvSim.opCode = 41
+	uvSim.operand = 25
+	uvSim.accumulator = 1234
+	uvSim.memory[1] = +1234
+
+	uvSim.loadNextInstruction()
+
+	expect(uvSim.instructionCounter).toBe(1)
+	expect(uvSim.instructionRegister).toBe(1234)
+	expect(uvSim.opCode).toBe(12)
+	expect(uvSim.operand).toBe(34)
+})
+
+test('load next branchZero branch instruction', () => {
+	uvSim.instructionCounter = 0
+	uvSim.opCode = 42
+	uvSim.operand = 25
+	uvSim.accumulator = 0
+	uvSim.memory[25] = +1234
+
+	uvSim.loadNextInstruction()
+
+	expect(uvSim.instructionCounter).toBe(25)
+	expect(uvSim.instructionRegister).toBe(1234)
+	expect(uvSim.opCode).toBe(12)
+	expect(uvSim.operand).toBe(34)
+})
+
+test('load next branchZero no branch instruction', () => {
+	uvSim.instructionCounter = 0
+	uvSim.opCode = 42
+	uvSim.operand = 25
+	uvSim.accumulator = 1234
+	uvSim.memory[1] = +4321
+
+	uvSim.loadNextInstruction()
+
+	expect(uvSim.instructionCounter).toBe(1)
+	expect(uvSim.instructionRegister).toBe(4321)
+	expect(uvSim.opCode).toBe(43)
+	expect(uvSim.operand).toBe(21)
 })
 
 test('read puts number into memory', () => {
-	uvsim.read(10, 20)
-	expect(uvsim.memory[10]).toBe(20)
+	uvSim.read(10, 20)
+	expect(uvSim.memory[10]).toBe(20)
 })
 
 test('write console logs correct value', () => {
 	const consoleSpy = jest.spyOn(console, 'log')
-	uvsim.write(10)
+	uvSim.write(10)
 	expect(consoleSpy).toHaveBeenCalledWith('00020')
 })
 
 test('load puts correct number into accumulator', () => {
-	uvsim.load(10)
-	expect(uvsim.accumulator).toBe(20)
+	uvSim.load(10)
+	expect(uvSim.accumulator).toBe(20)
 })
 
 test('store puts number in accumulator into correct memory location', () => {
-	uvsim.store(11)
-	expect(uvsim.memory[11]).toBe(20)
+	uvSim.store(11)
+	expect(uvSim.memory[11]).toBe(20)
 })
 
-test('+1010 is valid instruction', () => {
-	expect(uvsim.validateInstruction('+1010')).toBe(true)
+test('test add generates correct value', () => {
+	uvSim.accumulator = 10
+	uvSim.memory[10] = 20
+
+	uvSim.add(10)
+
+	expect(uvSim.accumulator).toBe(30)
 })
 
-test('+10101 is invalid instruction', () => {
-	expect(uvsim.validateInstruction('+10101')).toBe(false)
+test('test subtract generates correct value', () => {
+	uvSim.accumulator = 40
+	uvSim.memory[10] = 15
+
+	uvSim.subtract(10)
+
+	expect(uvSim.accumulator).toBe(25)
 })
 
-afterAll(() => uvsim.prompts.close())
+test('test multiply generates correct value', () => {
+	uvSim.accumulator = 5
+	uvSim.memory[10] = 10
+
+	uvSim.multiply(10)
+
+	expect(uvSim.accumulator).toBe(50)
+})
+
+test('test divide generates correct value', () => {
+	uvSim.accumulator = 40
+	uvSim.memory[10] = 2
+
+	uvSim.divide(10)
+
+	expect(uvSim.accumulator).toBe(20)
+})
+
+afterAll(() => uvSim.prompts.close())
